167a534cd5f36bbc6954403b7e489f04c41aa587
Aravind Yarram <yaravind@gmail.com>
2026-01-01 09:18:56 -0500

    Fix #12

---
+++ git_setup.sh
@@ -0,0 +1,10 @@
+echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
+read full_name
+
+echo "Type in your email address (the one used for your GitHub account): "
+read email
+
+git config --global user.email "$email"
+git config --global user.name "$full_name"
+
+echo "👌 Awesome, all set."
